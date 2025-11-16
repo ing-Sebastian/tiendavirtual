8eaef5ed64c21d94579c567d054eb57cb9466676
Ing. Sebastian <perezcanosebastianandres2304@gmail.com>
2025-11-16 02:01:15 -0500

    2 Commit - Login y Catalogo vistas
    Vista de login y catalogo iniciadas

---
+++ src/Views/mainClass.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Views;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Sebastian Perez
+ */
+public class mainClass extends Application{
+    
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        
+        stage.setTitle("Login");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
